e2865067aff7872d055fc1c489428e85213fa41d
MinChan Lee <shigatsu970704@gmail.com>
2021-01-13 15:00:07 +0900

    03.Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  console.log(count);
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "PLUS") {
+    return count + 1;
+  }
+  if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "PLUS" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
