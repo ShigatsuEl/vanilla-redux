b64c2e48d99bb1e7781d34f591090c1c3eaaec2b
MinChan Lee <shigatsu970704@gmail.com>
2021-01-13 14:45:48 +0900

    02.Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+  console.log(count);
+  return count;
 };
 
-const handlePlus = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
