ec41a3ab432b1360e967be46e6342d8b894ecb63
MinChan Lee <shigatsu970704@gmail.com>
2021-01-15 11:29:45 +0900

    13.MapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
-    console.log(text);
+    addToDo(text);
+    setText("");
   }
   return (
     <>
       <h1>To Dos</h1>
       <form onSubmit={onSubmit}>
-        <input type="text" placeholder="Write a to do" onChange={onChange} />
+        <input
+          type="text"
+          placeholder="Write a to do"
+          value={text}
+          onChange={onChange}
+        />
         <button>Add</button>
       </form>
       <ul>{JSON.stringify(toDos)}</ul>
@@ -26,4 +33,10 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
