9a437688215f70c213a857de6389d4edb14f8346
MinChan Lee <shigatsu970704@gmail.com>
2021-01-17 11:34:24 +0900

    18.Redux Toolkit's ConfigureStore & CreateSlice

--- src/store.js
+++ src/store.js
@@ -1,38 +1,22 @@
-import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-/* const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      console.log(action);
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-}; */
-
-// createReducer를 사용하면 state를 mutate하기 쉽게 만들어준다.
-// Redux Toolkit은 mutate한 state를 가져가서 뒤에서 우리가 예전에 했던 unmutate한 상태로 바꿔준다. 이것은 Redux Toolkit이 immer이란 것 뒤에서 동작하기 때문이다.
-// push는 mutate한 메서드이기 때문에 return값이 없어 state를 return하게 되면 문제가 생긴다. 반면에 filter같은 unmutate한 메서드는 return값이 반환하기 때문에 state를 return해줘도 문제가 생기지 않는다.
-// 즉, state object를 mutate를 하거나 새로운 state object를 리턴하는 것! 둘을 잘 비교해서 사용해야 한다.
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
+// createSlice()메서드는 reducer를 생성해줄 뿐만 아니라 action도 같이 만들어준다. 제일 짧은 코드로 state를 관리할 수 있음
+// 궁금하면 console로 toDos.reducer & toDos.actions 찍어보기 바람
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = createStore(reducer);
+// configureStore를 사용해 store를 생성하게 되면 configureStore 옵션 중 devTools가 자동으로 설정되어 React Dev Tools가 자동적으로 켜지는 것을 확인할 수 있다.
+const store = configureStore({ reducer: toDos.reducer });
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+export const { add, remove } = toDos.actions;
 
 export default store;
